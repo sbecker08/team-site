6ca8c892bf077203fcd9b5242bb2e6b3a3a0f86a
Shaun Becker <Shaun.Becker@marcusmillichap.com>
2023-05-16 15:46:38 -0700

    Fixes for accessibility

--- component/AboutUs/AboutUs.js
+++ component/AboutUs/AboutUs.js
@@ -12,7 +12,7 @@ function HeroBanner({ children }){
                         </h2>                        
                         <EditableText text="In order to best serve our clients, we provide a truly holistic approach to their investment needs. We advise and assist them in executing their respective business plans, and help them overcome obstacles they run into along the way. Due to our state of the art research and analytical capabilities"></EditableText>
                     </div>
-                    <img className={styles.aboutUsImage} src="./team.webp" />
+                    <img alt="Team Picture" className={styles.aboutUsImage} src="./team.webp" />
                 </div>
             </div>
         </div>
